bdfc5e0ed1df615bee298b67c306534ccd48e61f
Emmett Miller <emmett@invezo.com>
2024-03-05 17:18:54 -0700

    redirect all docs.multibase.co

--- next.config.js
+++ next.config.js
@@ -1,8 +1,30 @@
 const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.tsx',
 })
 
 module.exports = withNextra({
-  basePath: '/docs',
+    basePath: '/docs',
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                destination: '/docs/:path*',
+                basePath: false,
+                permanent: true,
+                has: [
+                    {
+                        type: 'host',
+                        value: 'docs.multibase.co'
+                    }
+                ]
+            }
+            // {
+            //   source: '/',
+            //   destination: '/docs',
+            //   basePath: false,
+            //   permanent: false
+            // }
+        ]
+    }
 })
